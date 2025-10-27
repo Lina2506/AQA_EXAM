572ec92a77ecf8cbf7805106249b9f3cd2db05e1
Lina2506 <allarenotme.lina@gmail.com>
2025-10-27 17:40:26 +0200

    create class BaseTests

---
+++ src/main/java/org/base/config/BaseTests.java
@@ -0,0 +1,38 @@
+package org.base.config;
+
+import com.codeborne.selenide.*;
+import org.testng.annotations.*;
+
+public class BaseTests {
+    @BeforeTest
+    public void config() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1024x768";
+        Configuration.holdBrowserOpen = true;
+        Configuration.headless = false;
+        Configuration.timeout = 10000;
+        Configuration.savePageSource = false;
+        Configuration.screenshots = true;
+    }
+    @BeforeSuite
+    public void createUserBeforeSuite() {
+
+    }
+    @AfterSuite
+    public void deleteUserAfterSuite() {
+
+    }
+    @BeforeMethod
+    public void openMainPage() {
+        Selenide.open("https://www.demoblaze.com/");
+    }
+    @AfterMethod
+    public void clearWebDriver() {
+//        Selenide.clearBrowserCookies();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+}
