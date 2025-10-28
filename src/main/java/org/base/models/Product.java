823bcc286e1e5cbda77f569564e7fbc3ac37af9c
Lina2506 <allarenotme.lina@gmail.com>
2025-10-28 09:53:22 +0200

    create models for User and Product

---
+++ src/main/java/org/base/models/Product.java
@@ -0,0 +1,10 @@
+package org.base.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String productName;
+    private Integer productPrice;
+    private String productDescription;
+}
---
+++ src/main/java/org/base/models/User.java
@@ -0,0 +1,9 @@
+package org.base.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String password;
+}
