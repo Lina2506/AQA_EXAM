65f7dab0d5df2b87a5409bb2a346f5dd08d1896c
Lina2506 <allarenotme.lina@gmail.com>
2025-10-29 09:29:12 +0200

    credentials are added in Constants

--- src/main/java/org/base/helpers/Constants.java
+++ src/main/java/org/base/helpers/Constants.java
@@ -4,5 +4,10 @@ public class Constants {
     public static final String BASE_URL = "https://www.demoblaze.com/";
     public static final String USERNAME="Polina_test";
     public static final String PASSWORD="Polina_test";
-
+    public static final String ORDER_NAME="Polina";
+    public static final String OTDER_Country="Ukraine";
+    public static final String OTDER_City="Kyiv";
+    public static final String OTDER_Creditcard="12345678";
+    public static final String OTDER_Month="september";
+    public static final String OTDER_Year="2025";
 }
