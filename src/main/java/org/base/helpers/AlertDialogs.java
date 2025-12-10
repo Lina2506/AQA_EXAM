d74eb40f1bcc94b7c8d7cc2795d08970bbbc9e69
Lina2506 <allarenotme.lina@gmail.com>
2025-12-10 10:05:29 +0200

    removed asserts from AlertDialogs

--- src/main/java/org/base/helpers/AlertDialogs.java
+++ src/main/java/org/base/helpers/AlertDialogs.java
@@ -1,7 +1,6 @@
 package org.base.helpers;
 
 import com.codeborne.selenide.Selenide;
-import org.testng.Assert;
 
 public class AlertDialogs {
     public static String getAlertText() {
@@ -9,14 +8,5 @@ public class AlertDialogs {
     }
     public static void acceptAlert() {
         Selenide.switchTo().alert().accept();
-    }
-    public static void verifyAlertText(String text) {
-        String actualText = getAlertText();
-        Assert.assertTrue(actualText.contains(text));
-        acceptAlert();
-    }
-    public static void closeAlert() {
-        String text = getAlertText();
-        acceptAlert();
     }
 }
